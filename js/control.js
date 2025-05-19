70538b0f4e9225930b869e27a111b8d861a92e4f
EltonNushi <elton_nushi@yahoo.co.uk>
2025-05-19 15:57:36 +0100

    Update control.js

--- js/control.js
+++ js/control.js
@@ -29,7 +29,8 @@ app = new Vue({
         updateEMT: function(event) {
             //target = event.path[0].getAttribute("value")
             const state = event.currentTarget?.getAttribute("value")
-            type = event.path[0].getAttribute("type")
+            const state = event.currentTarget?.getAttribute("type")
+            //type = event.path[0].getAttribute("type")
             app.core.emt_current.src = target
             app.core.emt_current.type = type
             app.core.emt.forEach(function(el) {
